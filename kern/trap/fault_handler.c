eaf3d702463c52ec3ec99cc7485faf3d1dad0e88
Seif Omran <seifomran1324@gmail.com>
2022-12-06 05:16:05 +0200

    Finish Fault Handler

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -82,12 +82,10 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 
 	//refer to the project presentation and documentation for details
 
-	cprintf("Enter @%x, %x\n", fault_va, ROUNDDOWN(fault_va, PAGE_SIZE));
 	uint32 currSize = env_page_ws_get_size(curenv);
 	uint32 maxSize = curenv->page_WS_max_size;
 
 	if(currSize < maxSize) {
-		cprintf("curr %d \t max %d\n", currSize, maxSize);
 		// Placement
 		uint32 *pageTablePtr = NULL;
 		struct FrameInfo *allocatedFrame = get_frame_info(curenv->env_page_directory, fault_va, &pageTablePtr);;
@@ -106,16 +104,6 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 			}
 
 		}
-		else {
-
-
-			cprintf("Exists\n");
-		}
-		//env_page_ws_set_entry(curenv, curenv->page_last_WS_index, fault_va);
-
-		//env_page_ws_print(curenv);
-		//env_page_ws_set_entry(curenv, curenv->page_last_WS_index, fault_va);
-		//curenv->page_last_WS_index++;
 
 		int found = 0;
 		for(int i = 0; i < curenv->page_WS_max_size; i++) {
@@ -128,89 +116,62 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 			}
 		}
 		if(found == 0) {
-			//env_page_ws_print(curenv);
-			//cprintf("entry: %d, max: %d\n", curenv->page_last_WS_index, curenv->page_WS_max_size);
+
 			env_page_ws_set_entry(curenv, curenv->page_last_WS_index, fault_va);
 			curenv->page_last_WS_index = (curenv->page_last_WS_index + 1) % curenv->page_WS_max_size;
 		}
-		//uint32 *ptrToPageTable = NULL;
-//		allocatedFrame = get_frame_info(curenv->env_page_directory, fault_va, &ptrToPageTable);
-		//pf_update_env_page(curenv, ROUNDDOWN(fault_va, PAGE_SIZE), allocatedFrame);
-
 	}
 	else {
 		// Replacement
 		uint32 victimVA = 0;
 
-		while(1) {
+		while(1 == 1) {
+			uint32 pageAddress = env_page_ws_get_virtual_address(curenv, curenv->page_last_WS_index);
+			int isUsed = (pt_get_page_permissions(curenv->env_page_directory, pageAddress) & PERM_USED) ? 1 : 0;
+			if(isUsed == 0) {
+				victimVA = pageAddress;
 
-			int f00 = 0;
-			for(int i = 0; i < curenv->page_WS_max_size; i++) {
-				uint32 pageAddress = env_page_ws_get_virtual_address(curenv, i);
-				int isModified = (pt_get_page_permissions(curenv->env_page_directory, pageAddress) & PERM_MODIFIED) ? 1 : 0;
-				int isUsed = (pt_get_page_permissions(curenv->env_page_directory, pageAddress) & PERM_USED) ? 1 : 0;
-
-				if(isUsed == 0 && isModified == 0) {
-					victimVA = pageAddress;
-					f00 = 1;
-					break;
-				}
-			}
-			if (f00 == 1) {
 				break;
 			}
 			else {
-				int f01 = 0;
-				for(int i = 0; i < curenv->page_WS_max_size; i++) {
-					uint32 pageAddress = env_page_ws_get_virtual_address(curenv, i);
-					int isUsed = (pt_get_page_permissions(curenv->env_page_directory, pageAddress) & PERM_USED) ? 1 : 0;
-
-					if(isUsed == 0) {
-						victimVA = pageAddress;
-						f01 = 1;
-						break;
-					}
-					else {
-						pt_set_page_permissions(curenv->env_page_directory, pageAddress, 0, PERM_USED);
-					}
-				}
-				if (f01 == 1) {
-					break;
-				}
+				pt_set_page_permissions(curenv->env_page_directory, pageAddress, 0, PERM_USED);
 			}
 
+			curenv->page_last_WS_index = ((curenv->page_last_WS_index + 1) % curenv->page_WS_max_size);
 
 		}
 
+
+
+		int isVictimModified = (pt_get_page_permissions(curenv->env_page_directory, victimVA) & PERM_MODIFIED) ? 1 : 0;
+
+		if(isVictimModified == 0) {
+			env_page_ws_invalidate(curenv, victimVA);
+		}
+		else {
+
+			uint32 *pt = NULL;
+			struct FrameInfo *updatedPage = get_frame_info(curenv->env_page_directory, victimVA, &pt);
+			pf_update_env_page(curenv, victimVA, updatedPage);
+			env_page_ws_invalidate(curenv, victimVA);
+		}
+
+		uint32 *pageTablePtr = NULL;
+		struct FrameInfo *allocFrame = get_frame_info(curenv->env_page_directory, victimVA, &pageTablePtr);
+		int mapres = map_frame(curenv->env_page_directory, allocFrame, fault_va, PERM_WRITEABLE | PERM_USER);
+		unmap_frame(curenv->env_page_directory, victimVA);
+		uint32 readStatus = pf_read_env_page(curenv, ((void *)fault_va));
 
 
 		for(int i = 0; i < curenv->page_WS_max_size; i++) {
-			int wspva = env_page_ws_get_virtual_address(curenv, i);
+			int emptyEntry = env_page_ws_is_entry_empty(curenv, i);
 
-			if (wspva == victimVA) {
-				int isVictimModified = (pt_get_page_permissions(curenv->env_page_directory, victimVA) & PERM_MODIFIED) ? 1 : 0;
-
-				if(isVictimModified == 1) {
-					if((victimVA > USER_HEAP_START && victimVA < USER_HEAP_MAX) || (victimVA < USTACKTOP && victimVA > USTACKBOTTOM)) {
-						uint32 *pt = NULL;
-						struct FrameInfo *updatedPage = get_frame_info(curenv->env_page_directory, victimVA, &pt);
-						pf_update_env_page(curenv, victimVA, updatedPage);
-					}
-
-				}
-
-				env_page_ws_invalidate(curenv, victimVA);
-				uint32 *pageTablePtr = NULL;
-				struct FrameInfo *allocFrame = get_frame_info(curenv->env_page_directory, victimVA, &pageTablePtr);
-				int mapres = map_frame(curenv->env_page_directory, allocFrame, fault_va, PERM_WRITEABLE | PERM_USER);
+			if(emptyEntry == 1) {
 				env_page_ws_set_entry(curenv, i, fault_va);
-				curenv->page_last_WS_index = (i + 1) % curenv->page_WS_max_size;
+				curenv->page_last_WS_index = ((curenv->page_last_WS_index + 1) % curenv->page_WS_max_size);
 			}
 		}
-
 	}
-	cprintf("Out\n");
-
 }
 void __page_fault_handler_with_buffering(struct Env * curenv, uint32 roundedFaultedVA)
 {
